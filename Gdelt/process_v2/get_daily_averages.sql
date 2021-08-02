f0bffc612a898bbe0e8c0c4774c53969f2115fb5
Xavier Miles <xavier.miles@stats.govt.nz>
2021-08-02 04:20:23 +0000

    Now calculates tone depending on country where article was published

--- Gdelt/process_v2/get_daily_averages.sql
+++ Gdelt/process_v2/get_daily_averages.sql
@@ -1,19 +1,26 @@
 SELECT
-  date, countries, AVG(tone) as avg_tone
+  date, country,
+  CASE
+    WHEN country = 'AS' then source_name LIKE '%.au%'
+    ELSE source_name LIKE '%.' || LOWER(country) || '%'
+  END as from_onshore,  -- to weight onshore vs offshore articles differently
+  AVG(tone) as tone
 FROM
   (
     SELECT
-      DATE(datetime) as date, UNNEST(countries) as countries,
+      DATE(datetime) as date, UNNEST(countries) as country,
+      source_name,
       AVG(tone) as tone,
       pos, neg, wc
     FROM
       gdelt_raw
     GROUP BY
-      date, countries,
+      date, country,
+      source_name,
       pos, neg, wc  -- to control for syndication
   ) t1
 GROUP BY
-  date, countries
+  date, country, from_onshore
 ORDER BY
-  date, countries
+  date, country, from_onshore
 ;
