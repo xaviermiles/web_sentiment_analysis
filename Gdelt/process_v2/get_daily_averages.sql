4fe2929c560911a36376fcd539d8b0d10e76cd20
Xavier Miles <xavier.miles@stats.govt.nz>
2021-08-06 03:12:47 +0000

    Re-reformat SQL

--- Gdelt/process_v2/get_daily_averages.sql
+++ Gdelt/process_v2/get_daily_averages.sql
@@ -5,31 +5,31 @@ SELECT date,
        Avg(tone) AS avg_tone,
        Count(*)  AS num_articles
 FROM   (
-       -- Combine (intra-country) syndicated articles into one row
-       SELECT Date(datetime) AS date,
-              country,
-              from_onshore,
-              Avg(tone)      AS tone
-        FROM  (
-              -- Construct "from_onshore"
-              SELECT *,
-                     CASE
-                       WHEN country = 'AS' THEN source_name LIKE '%.au%'
-                       ELSE source_name LIKE '%.' || Lower(country) || '%'
-                     END AS from_onshore
-              FROM   (
-                      -- Expand "countries"
-                      SELECT *,
-                             Unnest(countries) AS country
-                      FROM   gdelt_raw
-                      WHERE  Date(datetime) > '2021-07-26' -- FOR QUICKER TESTING
-                      ) t1
-               ) t2
-        GROUP BY date,
-                 country,
-                 from_onshore,
-                 pos, neg, wc -- to control for syndication
-       ) t3
+    -- Combine (intra-country) syndicated articles into one row
+    SELECT Date(datetime) AS date,
+          country,
+          from_onshore,
+          Avg(tone)      AS tone
+    FROM (
+        -- Construct "from_onshore"
+        SELECT *,
+               CASE
+                 WHEN country = 'AS' THEN source_name LIKE '%.au%'
+                 ELSE source_name LIKE '%.' || Lower(country) || '%'
+               END AS from_onshore
+        FROM (
+            -- Expand "countries"
+            SELECT *,
+                   Unnest(countries) AS country
+            FROM   gdelt_raw
+            WHERE  Date(datetime) > '2021-07-26' -- FOR QUICKER TESTING
+        ) t1
+    ) t2
+    GROUP BY date,
+             country,
+             from_onshore,
+             pos, neg, wc -- to control for syndication
+) t3
 GROUP  BY date,
           country,
           from_onshore
